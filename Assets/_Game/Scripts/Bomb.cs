005d9cfd24a5b73d8b1260ac3b2f4f58dbfac278
J-Eugenio <eugeniopb4@gmail.com>
2022-06-22 15:12:14 -0300

    feat: ajustes finais do objeto bomba

--- Assets/_Game/Scripts/Bomb.cs
+++ Assets/_Game/Scripts/Bomb.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float speed, startForce;
+    [SerializeField] private GameObject beamLight;
+    private Rigidbody2D myRB;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRB = this.gameObject.GetComponent<Rigidbody2D>();
+        ApplyForce();
     }
 
     // Update is called once per frame
@@ -17,9 +20,20 @@ public class Bomb : MonoBehaviour
         Rotate();
     }
 
+    private void ApplyForce(){
+        myRB.AddForce(transform.up * startForce, ForceMode2D.Impulse);
+    }
     private void Rotate(){
-        
         //Random.Range(0, speed)
-        transform.Rotate(new Vector3(0, speed, 0f) * Time.deltaTime);
+        transform.Rotate(new Vector3(Random.Range(0, speed), speed, 0f) * Time.deltaTime);
+    }
+
+    public void BombGameOver(){
+        speed = 0f;
+        myRB.bodyType = RigidbodyType2D.Kinematic;
+        myRB.simulated = false;
+        CircleCollider2D myCollider = this.gameObject.GetComponent<CircleCollider2D>();
+        myCollider.enabled = false;
+        GameObject tempBeamLight = Instantiate(beamLight, this.transform.position, Quaternion.identity) as GameObject;
     }
 }
